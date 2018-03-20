a72e5b3adbadc5af5232ba755315ab75ec7beae2
javaxd123 <marcinwarzybok@outlook.com>
2018-03-20 22:14:19 +0100

    After make first post to the server

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions/index';
 
 class PostsNew extends React.Component {
 
     renderField(field) {
+        const { meta: { touched, error } } = field
+        const className = `form-group ${touched && error ? 'has-danger' : ''}`
+
         return (
-            <div className="form-group">
+            <div className={className}>
                 <label>{field.label}</label>
                 <input
                     className="form-control"
                     type="text"
                     {...field.input}
                 />
+                <div className="text-help">
+                    {touched ? error : ''}
+                </div>
 
-                {field.meta.touched ?
-                    <div className="error">{field.meta.error}</div> :
-                    ''}
             </div>
         )
     }
 
     onSubmit(values) {
-        console.log(values)
+        this.props.createPost(values);
     }
 
     render() {
@@ -46,6 +52,7 @@ class PostsNew extends React.Component {
                     component={this.renderField}
                 />
                 <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
             </form>
         )
     }
@@ -66,11 +73,12 @@ function validate(values) {
         errors.content = "Enter some content please!!!";
     }
 
-
     return errors
 }
 
 export default reduxForm({
     validate,
     form: 'PostsNewForm'
-})(PostsNew);
+})(
+    connect(null, { createPost })(PostsNew)
+);
