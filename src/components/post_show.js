7777940057120bb155903e9c4b77a7b94daea107
javaxd123 <marcinwarzybok@outlook.com>
2018-03-21 23:18:09 +0100

    create PostShow

---
+++ src/components/post_show.js
@@ -0,0 +1,11 @@
+import React from 'react' ;
+
+class PostsShow extends React.Component {
+    render() {
+        return (
+            <div>Show Post!!</div>
+        )
+    }
+}
+
+export default PostsShow;
--- src/reducers/posts_reducer.js
+++ src/reducers/posts_reducer.js
@@ -1,10 +1,13 @@
-import { FETCH_POSTS } from '../actions/index';
+import { FETCH_POSTS, FETCH_POST } from '../actions/index';
 import _ from 'lodash';
 
 export default function(state = {}, action) {
     switch (action.type) {
         case FETCH_POSTS:
             return _.mapKeys(action.payload.data, 'id')
+        case FETCH_POST:
+            const post = action.payload.data;
+            return {...state, }
         default:
             return state
     }
