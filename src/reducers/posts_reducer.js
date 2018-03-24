fd5acda6f018821e4ab7c8382fa744f211269fb6
javaxd123 <marcinwarzybok@outlook.com>
2018-03-24 10:42:53 +0100

    Finish App

--- src/reducers/posts_reducer.js
+++ src/reducers/posts_reducer.js
@@ -1,16 +1,18 @@
-import { FETCH_POSTS, FETCH_POST } from '../actions/index';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions/index';
 import _ from 'lodash';
 
 export default function (state = {}, action) {
+    console.log(state,"stan główny")
     switch (action.type) {
+        case DELETE_POST:
+            console.log(state,"usuwanie")
+            return _.omit(state, action.payload)
         case FETCH_POSTS:
             return _.mapKeys(action.payload.data, 'id')
         case FETCH_POST:
             const post = action.payload.data;
             const newState = { ...state }
-            console.log(newState)
             newState[post.id] = post
-            console.log(newState[post.id])
             return newState
         default:
             return state
